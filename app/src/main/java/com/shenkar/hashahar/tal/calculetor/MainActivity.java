06186a4645655fbf5e51dcba055818648e62fac8
Talhashahar <talhashahar@gmail.com>
2018-03-27 15:46:46 +0300

    add onclick for all nunmber and add them to screen. add onclick for "plus" button and add to screen.

--- app/src/main/java/com/shenkar/hashahar/tal/calculetor/MainActivity.java
+++ app/src/main/java/com/shenkar/hashahar/tal/calculetor/MainActivity.java
@@ -12,6 +12,18 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private TextView mScreen;
     private Button mButtonOne;
     private Button mButtonTwo;
+    private Button mButtonThree;
+    private Button mButtonFour;
+    private Button mButtonFive;
+    private Button mButtonSix;
+    private Button mButtonSeven;
+    private Button mButtonNine;
+    private Button mButtonTen;
+
+    private Button mButtonPlus;
+
+    private  int Number1;
+    private  int Number2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,10 +34,28 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         mScreen = (TextView) findViewById(R.id.screen);
         mButtonOne = (Button) findViewById(R.id.button1);
         mButtonTwo = (Button) findViewById(R.id.button2);
+        mButtonThree = (Button) findViewById(R.id.button3);
+        mButtonFour = (Button) findViewById(R.id.button4);
+        mButtonFive = (Button) findViewById(R.id.button5);
+        mButtonSix = (Button) findViewById(R.id.button6);
+        mButtonSeven = (Button) findViewById(R.id.button7);
+        mButtonNine = (Button) findViewById(R.id.button8);
+        mButtonTen = (Button) findViewById(R.id.button9);
+
+        mButtonPlus = (Button) findViewById(R.id.buttonPlus);
 
 
         mButtonOne.setOnClickListener(this);
         mButtonTwo.setOnClickListener(this);
+        mButtonThree.setOnClickListener(this);
+        mButtonFour.setOnClickListener(this);
+        mButtonFive.setOnClickListener(this);
+        mButtonSix.setOnClickListener(this);
+        mButtonSeven.setOnClickListener(this);
+        mButtonNine.setOnClickListener(this);
+        mButtonTen.setOnClickListener(this);
+
+        mButtonPlus.setOnClickListener(this);
 
 
     }
@@ -33,7 +63,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View view) {
         if(view instanceof Button) {
-            mScreen.setText(((Button)view).getText());
+            if ((Button)view == mButtonPlus)
+            {
+
+                Number1 = Integer.parseInt((String) mScreen.getText());
+            }
+            String temp = (String) mScreen.getText();
+            mScreen.setText(temp.concat((String)((Button)view).getText()));
         }
     }
 }
